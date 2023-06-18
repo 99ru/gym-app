5f21ea39a337aa688395ac9c7e4e962bcba73bed
99ru <yan.99ru@gmail.com>
2023-06-18 18:49:23 +0200

    some styling to card

--- components/WorkoutCard.tsx
+++ components/WorkoutCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Card, CardBody } from "@windmill/react-ui";
 import { BsFillPencilFill } from "react-icons/bs";
 import { FiChevronDown } from "react-icons/fi";
-import EditDialog from "./EditWorkoutCard";
+import EditWorkoutCard from "./EditWorkoutCard";
 
 type Props = {
   workout: Workout;
@@ -34,56 +34,65 @@ const WorkoutCard: React.FC<Props> = ({ workout }) => {
     setEditing(false);
   };
 
-  return (
-    <div>
-      <Card className="p-4 m-2 rounded w-72">
-        <CardBody className="flex items-center">
-          <div className="mr-4">
-            <Image src={workout.image} alt={workout.name} width={40} height={40} />
-          </div>
-          <div>
-            <p className="font-bold">{workout.name}</p>
-            <button onClick={() => setExpanded(!isExpanded)}>
-              <FiChevronDown style={{ transform: `rotate(${isExpanded ? 180 : 0}deg)` }} />
-            </button>
-            {isExpanded && (
-              <div>
-                {workoutSets.map((set, index) => (
-                  <div
-                    key={index}
-                    className="bg-e5edff p-2 rounded-lg my-2 flex items-center justify-between"
-                  >
-                    <p className="text-lg">
-                      Set {index + 1}: {set.reps} reps, {set.weight} kg
-                    </p>
-                    <button onClick={() => handleEditSet(index)}>
-                      <BsFillPencilFill />
-                    </button>
-                  </div>
-                ))}
-                <button
-                  onClick={() => {
-                    setWorkoutSets([...workoutSets, { reps: 0, weight: 0 }]);
-                    setTotalSets(totalSets + 1);
-                  }}
+return (
+  <div>
+    <Card className="p-4 m-2 rounded min-w-[700px]">
+      <CardBody className="flex items-center">
+        <div className="mr-4">
+          <Image
+            src={workout.image}
+            alt={workout.name}
+            width={100}
+            height={40}
+          />
+        </div>
+        <div>
+          <p className="font-bold">{workout.name}</p>
+          <button onClick={() => setExpanded(!isExpanded)}>
+            <FiChevronDown
+              style={{ transform: `rotate(${isExpanded ? 180 : 0}deg)` }}
+            />
+          </button>
+          {isExpanded && (
+            <div >
+              {workoutSets.map((set, index) => (
+                <div 
+                  key={index}
+                  className="bg-e5edff p-2 rounded-lg my-2 flex items-center justify-between space-x-2 bg-slate-50"
                 >
-                  Add Set
-                </button>
-                {isEditing && (
-                  <EditDialog
-                    reps={workoutSets[editingSetIndex as number].reps}
-                    weight={workoutSets[editingSetIndex as number].weight}
-                    onSave={handleSave}
-                    onClose={() => setEditing(false)}
-                  />
-                )}
-              </div>
-            )}
-          </div>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+                  <div className="flex items-center space-x-2 bg-whitesmoke ">
+                    <p className="text-lg ml-2 w-20">Set {index + 1}</p>
+                    <p className="text-lg ml-2 w-20">{set.weight}kg</p>
+                    <p className="text-lg ml-2 w-20">{set.reps} Reps</p>
+                  </div>
+                  <button onClick={() => handleEditSet(index)}>
+                    <BsFillPencilFill />
+                  </button>
+                </div>
+              ))}
+              <button
+                onClick={() => {
+                  setWorkoutSets([...workoutSets, { reps: 0, weight: 0 }]);
+                  setTotalSets(totalSets + 1);
+                }}
+              >
+                Add Set
+              </button>
+            </div>
+          )}
+        </div>
+      </CardBody>
+    </Card>
+    {isEditing && (
+      <EditWorkoutCard
+        reps={workoutSets[editingSetIndex as number].reps}
+        weight={workoutSets[editingSetIndex as number].weight}
+        onSave={handleSave}
+        onClose={() => setEditing(false)}
+      />
+    )}
+  </div>
+);
+}
 
 export default WorkoutCard;
