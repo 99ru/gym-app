17d094fb1169f2335a5290e1e9052d269a303635
99ru <yan.99ru@gmail.com>
2023-06-29 09:08:02 +0200

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,12 @@ import React, { useState } from "react";
 import workoutData from "../utils/workoutData";
 import AddWorkout from "../components/AddWorkout";
 import SelectMuscle from "../components/SelectMuscle";
-
 import { useUser } from '@clerk/nextjs'
 
 const Home: React.FC = () => {
 
-  const { data, error, selectedWorkouts, setSelectedWorkouts, onDelete } = workoutData();
+  const { data, selectedWorkouts, setSelectedWorkouts, onDelete } = workoutData();
   const [showWorkout, setShowWorkout] = useState(false);
-
-  if (error) return <div>Failed to load workouts</div>;
-  if (!data) return <div>Loading...</div>;
 
   return (
     <>
