2ed84e21dbc8855824a184c0b18a9d22f64ce4bf
99ru <yan.99ru@gmail.com>
2023-07-22 21:34:48 +0200

    removed firebaseui because of netlify issues

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,8 @@
-'use client'
+"use client";
 import React, { useContext } from "react";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import Image from "next/image";
 import { AuthContext } from "@/auth/AuthProvider";
-import { auth, signInAsGuest } from "@/utils/firebase";
+import { auth, signInAsGuest, signInWithGoogle } from "@/utils/firebase";
 
 const LoginPage = () => {
   const { setCurrentUser } = useContext(AuthContext);
@@ -17,20 +16,37 @@ const LoginPage = () => {
     }
   };
 
-  const uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      {
-        provider: "google.com",
-        scopes: ["openid", "email", "profile"],
-        customParameters: {
-          prompt: "select_account",
-        },
-      },
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
+  const handleGoogleLogin = async () => {
+    try {
+      const userCredential = await signInWithGoogle();
+      setCurrentUser(userCredential.user);
+    } catch (error) {
+      console.error("Error signing in with Google: ", error);
+    }
+  };
+
+  const buttonStyles = {
+    backgroundColor: "black",
+    color: "white",
+    height: "50px",
+    padding: "0 20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "5px",
+    marginBottom: "10px",
+  };
+
+  const googleIconStyles = {
+    height: "20px",
+    width: "20px",
+    marginRight: "10px",
+  };
+
+  const anonymousIconStyles = {
+    height: "26px", 
+    width: "26px", 
+    marginRight: "10px",
   };
 
   return (
@@ -51,7 +67,6 @@ const LoginPage = () => {
             </div>
             <main className="flex items-center justify-center p-6 w-full md:w-1/2">
               <div className="text-gray-700 dark:text-gray-200">
-             
                 <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-center mb-3 md:mb-4 tracking-tighter leading-2">
                   GetFit
                 </h1>
@@ -59,25 +74,29 @@ const LoginPage = () => {
                   Start your fitness journey today!
                 </h2>
 
-                <div className="pt-2 md:pt-0">
-                  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-                </div>
-                <div className="flex justify-center items-center">
-                  <button 
-                    className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-anonymous firebaseui-id-idp-button" 
-                    onClick={handleLogin}
-                    style={{backgroundColor: 'black'}}
-                  >
-                    <span className="firebaseui-idp-icon-wrapper">
-                      <img
-                        className="firebaseui-idp-icon"
-                        alt=""
-                        src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/anonymous.png"
-                      />
-                    </span>
-                    <span className="firebaseui-idp-text firebaseui-idp-text-long">
-                      Continue as Guest
-                    </span>
+                <div
+                  className="pt-2 md:pt-0"
+                  style={{
+                    alignItems: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <button onClick={handleGoogleLogin} style={buttonStyles}>
+                    <img
+                      style={googleIconStyles}
+                      alt=""
+                      src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                    />
+                    <span>Sign in with Google</span>
+                  </button>
+                  <button onClick={handleLogin} style={buttonStyles}>
+                    <img
+                      style={anonymousIconStyles}
+                      alt=""
+                      src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/anonymous.png"
+                    />
+                    <span>Continue as Guest</span>
                   </button>
                 </div>
               </div>
