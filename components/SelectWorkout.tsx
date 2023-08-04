53c0505963ae42edc945e9fb7b366c941e1842c7
99ru <yan.99ru@gmail.com>
2023-08-04 10:47:03 +0200

    better dialog

--- components/SelectWorkout.tsx
+++ components/SelectWorkout.tsx
@@ -6,6 +6,7 @@ import MuscleButton from "./MuscleButton";
 import WorkoutDialog from "./dialogs/SelectWorkoutDialog";
 import { IoCloseSharp } from "react-icons/io5";
 import { formatISO } from "date-fns";
+import { Dialog, Transition } from "@headlessui/react";
 
 // Define muscle groups as enums
 export enum MuscleGroups {
@@ -86,33 +87,50 @@ const SelectWorkout: React.FC<Props> = ({
   // Using enum to define muscle groups
   const musclesGroups = Object.values(MuscleGroups);
 
-  return (
-    <section className="flex justify-center mt-20">
-      <div className="w-full max-w-2xl bg-white rounded-xl shadow-lg p-8 text-center relative">
-        <h1 className="font-bold text-2xl mb-6">Select a muscle group</h1>
-        <div className="flex flex-wrap justify-center mb-6">
-          {musclesGroups.map((muscle) => (
-            <MuscleButton
-              key={muscle}
-              muscle={muscle}
-              selectedMuscle={selectedMuscle}
-              onClick={handleMuscleButtonClick}
+ return (
+    <Transition appear show={true} as={React.Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={handleDialogClose}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <div className="inline-block w-full max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+            <Dialog.Title as="h1" className="font-bold text-2xl mb-6 text-center">
+              Select a muscle group
+            </Dialog.Title>
+            <div className="flex flex-wrap justify-center mb-6">
+              {musclesGroups.map((muscle) => (
+                <MuscleButton
+                  key={muscle}
+                  muscle={muscle}
+                  selectedMuscle={selectedMuscle}
+                  onClick={handleMuscleButtonClick}
+                />
+              ))}
+            </div>
+            <IoCloseSharp
+              onClick={handleDialogClose}
+              className="absolute top-2 right-2 cursor-pointer text-2xl text-gray-500"
             />
-          ))}
+            <WorkoutDialog
+              open={open}
+              selectedMuscle={selectedMuscle}
+              workouts={workouts}
+              onClose={handleDialogClose}
+              onWorkoutClick={handleAddWorkout}
+            />
+          </div>
         </div>
-        <IoCloseSharp
-          onClick={handleDialogClose}
-          className="absolute top-2 right-2 cursor-pointer text-2xl text-gray-500"
-        />
-        <WorkoutDialog
-          open={open}
-          selectedMuscle={selectedMuscle}
-          workouts={workouts}
-          onClose={handleDialogClose}
-          onWorkoutClick={handleAddWorkout}
-        />
-      </div>
-    </section>
+      </Dialog>
+    </Transition>
   );
 };
 
